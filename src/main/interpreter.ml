80b13d19d1c44db5109f6bcaf2a484175a0340a6
oracleofnj <jared.samet@aya.yale.edu>
2016-11-05 15:13:10 -0400

    Handle selections better

--- src/main/interpreter.ml
+++ src/main/interpreter.ml
@@ -18,7 +18,7 @@ module CellMap  = Map.Make(struct
       | o -> o
   end);;
 
-type cell_value = ExtendNumber of float |
+type cell_value = ExtendNumber of int |
                   Range of range |
                   EmptyValue |
                   Uncalculated
@@ -35,6 +35,10 @@ and  subrange   = {
   base_offset:               cell;
   subrange_dimensions:       dimen;
 }
+
+let dimensions_of_range = function
+    Variable(v) -> v.variable_dimensions
+  | Subrange(sr) -> sr.subrange_dimensions
 
 let check_val rg (Cell(r, c)) =
   let Dimensions(num_rows, num_cols) = rg.variable_dimensions in
@@ -67,57 +71,96 @@ let get_formula rg (Cell(r, c)) =
 
 let rec evaluate scope cell e =
   let eval_binop op = function
-      (ExtendNumber(f1), ExtendNumber(f2)) -> (match op with
-        Plus -> ExtendNumber(f1 +. f2)
-      | Minus -> ExtendNumber(f1 -. f2)
-      | Times -> ExtendNumber(f1 *. f2)
-      | Divide -> ExtendNumber(f1 /. f2)
+      (ExtendNumber(n1), ExtendNumber(n2)) -> (match op with
+        Plus -> ExtendNumber(n1 + n2)
+      | Minus -> ExtendNumber(n1 - n2)
+      | Times -> ExtendNumber(n1 * n2)
+      | Divide -> ExtendNumber(n1 / n2)
       | _ -> EmptyValue)
     | _ -> EmptyValue in
-  match e with
+
+  let eval_unop op = function
+      ExtendNumber(n) -> (match op with
+          Neg -> ExtendNumber(-n)
+        | _ -> EmptyValue)
+    | _ -> EmptyValue in
+
+  let resolve_slice dimension_len cell_index sl =
+    let resolve_index = function
+        Abs(e) -> let ExtendNumber(i) = (evaluate scope cell e) in i
+      | Rel(e) -> let ExtendNumber(i) = (evaluate scope cell e) in cell_index + i
+      | DimensionStart -> 0
+      | DimensionEnd -> dimension_len in
+    (match sl with
+       (Some idx1, Some idx2) -> ((resolve_index idx1), (resolve_index idx2))
+     | (Some idx, None) -> let i = resolve_index idx in (i, i+1)
+     | _ -> raise(Cyclic("Inconceivable!"))) in
+
+  let range_of_val = function
+      Range(r) -> r
+    | v -> Variable({variable_name = Transform.idgen (); variable_dimensions = Dimensions(1,1); values = ref (CellMap.add (Cell(0,0)) (v, Black) CellMap.empty); formulas=[]}) in
+
+  let rec val_of_val = function
+      Range(rg) ->
+      let Dimensions(rows, cols) = dimensions_of_range rg in
+      if (rows <= 0) || (cols <= 0) then EmptyValue else
+        (if (rows = 1) && (cols = 1) then (val_of_val (get_val scope rg (Cell(0,0)))) else
+           Range(rg))
+    | o -> o in
+
+  val_of_val (match e with
     Empty -> EmptyValue
-  | LitInt(i) -> ExtendNumber(float_of_int i)
-  | LitFlt(f) -> ExtendNumber(f)
+  | LitInt(i) -> ExtendNumber(i)
+  | LitFlt(f) -> ExtendNumber(int_of_float f)
   | BinOp(e1, op, e2) -> eval_binop op ((evaluate scope cell e1),(evaluate scope cell e2))
+  | UnOp(op, e1) -> eval_unop op (evaluate scope cell e1)
   | Ternary(cond, true_exp, false_exp) -> (match (evaluate scope cell cond) with
         EmptyValue -> EmptyValue
-      | ExtendNumber(0.) -> (evaluate scope cell false_exp)
+      | ExtendNumber(0) -> (evaluate scope cell false_exp)
       | _ -> (evaluate scope cell true_exp))
   | Id(s) -> Range(Variable(List.find (fun rng -> rng.variable_name = s) scope))
   | Selection(expr, sel) ->
-    let Range(Variable(rng)) = (evaluate scope cell expr) in
-    let Cell(formula_row, formula_col) = cell in
-    let resolve_index formula_index = function
-        Abs(LitInt(i)) -> i
-      | Rel(LitInt(i)) -> formula_index + i
-      | Rel(UnOp(Neg,LitInt(i))) -> formula_index - i
-      | _ -> raise(Cyclic("Inconceivable!")) in
-    get_val scope rng (match sel with
-          (Some (Some row_idx, None), Some (Some col_idx, None)) -> Cell(resolve_index formula_row row_idx, resolve_index formula_col col_idx)
-      )
-(*                   LitFlt of float |
-    LitString of string |
+    let rng = range_of_val (evaluate scope cell expr) in
+    let Cell(cell_row, cell_col) = cell in
+    let Dimensions(rows, cols) = dimensions_of_range rng in
+    (match sel with
+       (Some row_slice, Some col_slice) ->
+       let (row_start, row_end) = resolve_slice rows cell_row row_slice in
+       let (col_start, col_end) = resolve_slice rows cell_col col_slice in
+       Range(Subrange({base_range = rng; subrange_dimensions = Dimensions(row_end - row_start, col_end - col_start); base_offset = Cell(row_start, col_start)}))
+     | _ -> EmptyValue)
+
+(*  LitString of string |
     LitRange of (expr list) list |
     Id of string |
     Wild |
     UnOp of unop * expr |
     Switch of expr option * case list |
     Call of string * expr list |
-    Selection of expr * sel |
-    Precedence of expr * expr *)
-  | _ -> ExtendNumber(3.14159)
-
+                     Precedence of expr * expr *)
+  | _ -> ExtendNumber(-1))
 
 and get_val scope rg cell =
-  print_endline ("Looking for " ^ rg.variable_name ^ index_of_cell cell) ;
-  let (value, color) = check_val rg cell in match color with
-    White ->
-    let new_value = (evaluate scope cell (get_formula rg cell)) in
-    print_endline ("Finished calculating " ^ rg.variable_name ^ index_of_cell cell) ;
-    rg.values := CellMap.add cell (new_value, Black) !(rg.values) ; new_value
-  | Grey -> let Cell(r, c) = cell in
-    raise (Cyclic(rg.variable_name ^ "[" ^ string_of_int r ^ "," ^ string_of_int c ^ "]"))
-  | Black -> print_endline ("Found " ^ rg.variable_name ^ index_of_cell cell) ; value
+  match rg with
+    Variable(v) -> (
+      (* print_endline ("Looking for " ^ v.variable_name ^ index_of_cell cell) ; *)
+      let (value, color) = check_val v cell in match color with
+        White ->
+        let new_value = (evaluate scope cell (get_formula v cell)) in
+        (* print_endline ("Finished calculating " ^ v.variable_name ^ index_of_cell cell) ; *)
+        v.values := CellMap.add cell (new_value, Black) !(v.values) ; new_value
+      | Grey -> let Cell(r, c) = cell in
+        raise (Cyclic(v.variable_name ^ "[" ^ string_of_int r ^ "," ^ string_of_int c ^ "]"))
+      | Black ->
+        (* print_endline ("Found " ^ v.variable_name ^ index_of_cell cell) ;  *)
+        value)
+  | Subrange(sr) ->
+    let Cell(cell_r, cell_c) = cell in
+    let Cell(sr_r, sr_c) = sr.base_offset in
+    get_val scope sr.base_range (Cell(cell_r + sr_r, cell_c + sr_c))
+
+
+(* All stub code below this point *)
 
 (* from http://stackoverflow.com/questions/243864/what-is-the-ocaml-idiom-equivalent-to-pythons-range-function without the infix *)
 let zero_until i =
@@ -138,6 +181,20 @@ let tailrec_map f l =
     | x :: xs -> map_aux (f x :: acc) xs
   in
   map_aux [] l
+
+let rec string_of_val scope = function
+    ExtendNumber(cv) -> string_of_int cv
+  | EmptyValue -> "empty"
+  | Uncalculated -> "huh?"
+  | Range(rg) ->
+    let Dimensions(rows, cols) = dimensions_of_range rg in
+    let row_list = zero_until rows in
+    let col_list = zero_until cols in
+    let cart = cartesian row_list col_list in
+    "{" ^ (String.concat "; " (tailrec_map (string_of_cell scope rg) cart)) ^ "}"
+
+and string_of_cell scope rg (r,c) =
+  index_of_cell (Cell(r,c)) ^ ": " ^ string_of_val scope (get_val scope rg (Cell(r,c)))
 
 let interpret input =
   let ast_raw = Parser.program Scanner.token input in
@@ -160,35 +217,5 @@ let interpret input =
     let x_formulas = List.map x_formula (List.filter is_x_formula foo.body) in
     let x_dims = x_dimensions (List.hd (List.filter is_x_vardecl foo.body)) in
     let rg = {variable_name="x"; variable_dimensions = x_dims; values=ref CellMap.empty; formulas = x_formulas} in
-    let Dimensions(rows, cols) = x_dims in
-    let row_list = zero_until rows in let col_list = zero_until cols in
-    let cart = cartesian row_list col_list in
-    let string_of_cell (r,c) = "[" ^ string_of_int r ^ "," ^ string_of_int c ^ "]: " ^ (match get_val [rg] rg (Cell(r,c)) with
-          ExtendNumber(cv) -> string_of_float cv |
-          EmptyValue -> "empty" |
-          Uncalculated -> "huh?" |
-          Range(_) -> "some range here"
-      ) in
-    String.concat "\n" (tailrec_map string_of_cell cart)
+    string_of_val [rg] (Range(Variable(rg)))
   | _ -> "";;
-
-(*
-
-
-let rg = {
-  name = "mine";
-  dimensions = Dimensions(1,1);
-  values = CellMap.empty;
-  formulas = [];
-}
-
-let rg = {rg with values = CellMap.add (Cell(0,0)) (CellValue(3.14159), Black) rg.values};;
-
-let (value, color) = get_val rg (Cell(2,0)) in match color with
-  White -> print_endline "i don't know yet"
-| Grey -> print_endline "hold your horses, i'm still working"
-| Black -> match value with
-    CellValue(f) -> print_endline (string_of_float f)
-  | EmptyValue -> print_endline "empty"
-  | Uncalculated -> print_endline "i don't know yet"
-*)

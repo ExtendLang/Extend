6ea43f63f5e0e05287b1b7c85d60b25a3fa471d9
Nigel Schuster <nigel.schusters@googlemail.com>
2016-11-17 13:54:55 -0500

    Added more env variables to avoid copy paste

--- testscript.sh
+++ testscript.sh
@@ -1,21 +1,25 @@
 TESTDIR=./testcases
 TMP_DIR=./tmp
-INT_OUT=interpreter.out
-COMP_OUT=compiled.out
-RES_OUT=res.out
-COMP_F=comp
-LLVM_F=$COMP_F.ll
+INT_OUT=.i.out
+COMP_OUT=.c.out
+RES_OUT=.r.out
+LLVM_F=.ll
 BIN_F=$COMP_F.o
-make >/dev/null
 mkdir -p $TMP_DIR
 for f in $(ls $TESTDIR); do
-  ./main.byte -i $TESTDIR/$f &> $TMP_DIR/$INT_OUT
-  ./main.byte -c $TESTDIR/$f &> $TMP_DIR/$LLVM_F
-  lli-3.4 $TMP_DIR/$LLVM_F &> $TMP_DIR/$COMP_OUT
-  diff $TMP_DIR/$INT_OUT $TMP_DIR/$COMP_OUT &> $TMP_DIR/$RES_OUT
+  INTERPRETER_TARGET=$TMP_DIR/$f$INT_OUT
+  EXTEND_TARGET=$TMP_DIR/$f$LLVM_F
+  EXTEND_FILE=$TESTDIR/$f
+  COMPILED_OUTPUT=$TMP_DIR/$f$COMP_OUT
+  RESULT_OUTPUT=$TMP_DIR/$f$RES_OUT
+  ./main.byte -i $EXTEND_FILE &> $INTERPRETER_TARGET
+  ./main.byte -c $EXTEND_FILE &> $EXTEND_TARGET
+  lli-3.4 $EXTEND_TARGET arg1 &> $COMPILED_OUTPUT
+  diff $INTERPRETER_TARGET $COMPILED_OUTPUT &> $RESULT_OUTPUT
   if [ $? -eq 0 ]; then
       echo "PASSED ($f)"
   else
       echo "FAILED ($f)"
+      cat $RESULT_OUTPUT
   fi
 done

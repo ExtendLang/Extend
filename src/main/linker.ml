2d4564a4f9aae99f67f72baf514b7095b452ee08
Nigel Schuster <nigel.schusters@googlemail.com>
2016-12-10 13:22:14 -0500

    Linking math library

--- src/main/linker.ml
+++ src/main/linker.ml
@@ -9,6 +9,6 @@ let link xtndOut ast compiler outputFile =
         StringSet.empty) in
   let tmpChan = open_out tmpFilename in
   output_string tmpChan xtndOut; close_out tmpChan;
-  let call = (String.concat " " (compiler :: "-o" :: outputFile :: tmpFilename :: (getExterns ast))) in
+  let call = (String.concat " " (compiler :: "-o" :: outputFile :: tmpFilename :: (getExterns ast))) ^ " -lm" in
   let res = Sys.command call in
   Sys.remove tmpFilename; if res == 0 then () else raise Not_found

09c39619d89f65a19d537336c3b3680553f732a0
Nigel Schuster <nigel.schusters@googlemail.com>
2016-12-10 13:50:19 -0500

    Modified linker to work for travis

--- src/main/linker.ml
+++ src/main/linker.ml
@@ -1,14 +1,22 @@
 module StringSet = Set.Make(String)
 let link xtndOut ast compiler outputFile =
-  let tmpFilename = Filename.temp_file "" ".ll"
+  let tmpFilenameLL = Filename.temp_file "" ".ll"
+  and tmpFilenameC = Filename.temp_file "" ".o"
   and getExterns (_,_,extern) =
     StringSet.elements
       (Ast.StringMap.fold
         (fun key value store -> StringSet.add value.Ast.extern_fn_libname store)
         extern
         StringSet.empty) in
-  let tmpChan = open_out tmpFilename in
+  let tmpChan = open_out tmpFilenameLL in
   output_string tmpChan xtndOut; close_out tmpChan;
-  let call = (String.concat " " (compiler :: "-o" :: outputFile :: tmpFilename :: (getExterns ast))) ^ " -nodefaultlibs -lm" in
-  let res = print_endline call; Sys.command call in
-  Sys.remove tmpFilename; if res == 0 then () else raise Not_found
+  let call1 = (String.concat " " ("llc-3.8" :: "-filetype=obj" :: tmpFilenameLL :: "-o" :: tmpFilenameC :: []))
+  and call2 = (String.concat " " (compiler :: "-o" :: outputFile :: tmpFilenameC :: (getExterns ast))) ^ " -nodefaultlibs -lm" in
+  let resc1 = Sys.command call1 in
+  if resc1 == 0 then (
+    Sys.remove tmpFilenameLL;
+    let resc2 = Sys.command call2 in
+      Sys.remove tmpFilenameC;
+      if resc2 == 0 then () else raise Not_found
+    )
+  else (Sys.remove tmpFilenameC;raise Not_found)

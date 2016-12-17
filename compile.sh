037728d1981a2156feeaa29d3022432731a6e42e
Nigel Schuster <nigel.schusters@googlemail.com>
2016-12-17 13:46:34 -0500

    A lot of wrong paths make it work

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 LIB=stdlib.o
 RUNTIME=runtime.o
 COMPILER=clang
-$COMPILER -c src/stdlib/lib.c -o $LIB
-$COMPILER -c src/stdlib/runtime.c -o $RUNTIME
+#$COMPILER -c src/stdlib/lib.c -o $LIB
+#$COMPILER -c src/stdlib/runtime.c -o $RUNTIME
 ./main.byte -cc $COMPILER -c $1 -l
-rm $LIB
-rm $RUNTIME
+#rm $LIB
+#rm $RUNTIME
